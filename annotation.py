6b0e2208e4105f9451eab4bc7dde2013b3e7a7e7
阿部大五郎 <daigoro@d31.vpn.tut.ac.jp>
2021-09-22 14:52:51 +0900

    initial commit

---
+++ annotation.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import glob
+import os
+from utils.util import setup_logger, list_class
+
+logger = setup_logger(__name__)
+
+option = st.selectbox(
+    'choose a prediction file',
+    list(glob.glob('./predictions/*.jsonl'))
+)
+
+filename_an = f'{os.path.splitext(option)[0]}_an.jsonl'.replace('predictions','annotated')
+if os.path.exists(filename_an):
+    logger.info(f'annotated file exists in {filename_an}')
+    _df = pd.read_json(filename_an,orient='records',lines=True)
+else:
+    logger.info(f'annotated file not exists in {filename_an}')
+    _df = pd.read_json(option,orient='records',lines=True)
+
+if 'class' not in _df.columns:
+    _df['class']=list(range(len(_df)))
+
+def save_df():
+    _df['class'] = [st.session_state[f'{i}_c'] for i in range(len(_df))]
+    _df.to_json(filename_an,orient='records',lines=True,force_ascii=False)
+    logger.info(f'saved annotated file in {filename_an}')
+
+iter_se  = _df['tokens'].iteritems()
+while True:
+    try:
+        index, value = next(iter_se)
+        s1,s2,_ = value.split('[SEP]')
+        s1 = s1.lstrip('[CLS]')
+        st.header(s1)
+        st.header(s2)
+        key = f'{index}_c'
+        init_value = _df.loc[index]['class'] if _df.loc[index]['class'] != [] else []
+        st.multiselect('含意関係の分類',list_class,init_value,key=key,on_change=save_df)
+    except StopIteration:
+        break
---
+++ utils/util.py
@@ -0,0 +1,36 @@
+import logging
+import sys
+
+def setup_logger(name):
+    """Logger をセットアップする関数.
+    Args:
+        name (str): Logger の名前空間. __name__ を指定する.
+    Returns:
+        logger (logging.Logger): セットアップした Logger
+    """
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler with a INFO log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    # add the handlers to the logger
+    logger.addHandler(ch)
+    return logger
+
+_dict_class = {
+    'その他':('一致',),
+    '単語の置き換え':('同義・類義','上位・下位','含意・前提','全体・部分'),
+    'フレーズの置き換え':('同義・類義','上位・下位','含意・前提','全体・部分','品詞の変換','共参照・照応'),
+    '文構造の置き換え':('語順入れ替え','態の変化','修飾句削除','主辞削除','並列・従属節','集合・リスト','同格','関係節'),
+    '推論':('時間の推論','空間の推論','数の推論','暗黙の関係','その他の推論')
+}
+_dict_class = {k:[f'{k}:{vl}' for vl in v] for k,v in _dict_class.items()}
+list_class = sum(_dict_class.values(),[])
